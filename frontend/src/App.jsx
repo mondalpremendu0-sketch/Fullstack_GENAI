079f7b0799eedcc6e74bf438b840c557d795fb57
Premendu Mondal <mondalpremendu0@gmail.com>
2026-05-05 23:59:06 +0530

    register ui is done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,13 @@
+import Register from './Components/auth/Register/Register.jsx'
+//import ThreeBackground from './Components/Background/ThreeBackground.jsx'
+
+
 export default function App() {
   return (
-    <h1>App</h1>
+    <>
+
+      <Register />
+    </>
+
     )
 }
