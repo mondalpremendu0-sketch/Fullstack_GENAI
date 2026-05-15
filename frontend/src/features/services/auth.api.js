2e14977e7d94aca55dfeb1e8a4a18e318e670c92
Premendu Mondal <mondalpremendu0@gmail.com>
2026-05-15 17:57:23 +0530

    returning the data api

--- frontend/src/features/services/auth.api.js
+++ frontend/src/features/services/auth.api.js
@@ -6,22 +6,48 @@ const api = axios.create({
 });
 
 export const register = async ({firstname,lastname,email, password}) => {
-  const data = await api.post("auth/register",{
+  try {
+    const response = await api.post("auth/register",{
     firstname,lastname,email, password
   })
+  return response.data;
+  } catch (err) {
+    console.error('Error:', err);
+    
+  }
+  
+  
 }
 export const login = async ({email, password}) => {
-  const data = await api.post("auth/login",{
+  try {
+    const response = await api.post("auth/login",{
     email, password
   })
+  return response.data;
+  } catch (err) {
+    console.error('Error:', err);
+    
+  }
+  
 }
 export const getMe = async () => {
-  const data = await api.get("auth/getMe",{
+  try {
+    const response = await api.get("auth/getMe");
+    return response.data;
+  } catch (err) {
+    console.error('Error:', err);
     
-  })
+  }
+  
 }
 export const logout = async () => {
-  const data = await api.get("auth/logout",{
+  try {
+    const response = await api.get("auth/logout")
+    return response.data;
     
-  })
+  } catch (err) {
+    console.error('Error:', err);
+    
+  }
+  
 }
