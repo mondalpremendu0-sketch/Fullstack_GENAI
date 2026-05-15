14207bbe2b1ecae593d7fb5ae93ca34e0a765531
Premendu Mondal <mondalpremendu0@gmail.com>
2026-05-15 18:51:53 +0530

    implement ayth context

---
+++ frontend/src/features/auth/hooks/useAuthContext.js
@@ -0,0 +1,35 @@
+import {useContext} from "react";
+import {AuthContext} from '../auth.context.jsx';
+import {register,login,getMe,logout} from '../services/auth.api.js'
+
+
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  const {user,setUser,loading,setLoading} = context;
+  
+  const handleRegister = async ({firstname,lastname,email,password}) => {
+
+    setLoading(true);
+    const data = await register({firstname,lastname,email,password});
+    setUser(data.user);
+    setLoading(false);
+  }
+  
+  const handleLogin = async ({email,password}) => {
+
+    setLoading(true);
+    const data = await login({email,password});
+    setUser(data.user);
+    setLoading(false);
+  }
+  
+  const handlegetMe = async ({email,password}) => {
+
+    setLoading(true);
+    const data = await login({email,password});
+    setUser(data.user);
+    setLoading(false);
+  }
+
+}
