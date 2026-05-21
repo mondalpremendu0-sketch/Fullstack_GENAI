273b37c1c3955919a2d6a71fdab1e4221510953e
Premendu Mondal <mondalpremendu0@gmail.com>
2026-05-21 11:30:37 +0530

    implement pdf text extraction

--- backend/src/controllers/interView.controller.js
+++ backend/src/controllers/interView.controller.js
@@ -1,7 +1,16 @@
+const { PDFParse } = require ('pdf-parse');
 
 async function interviewController(req,res)
 {
+  const {resume} = req.file;
+  const resumeContent = new PDFParse({ data: req.file.buffer });
+  const resumeText = await resumeContent.getText();
+  //console.log(resumeText.text);
   
+  
+  res.json({ 
+    text:resumeText.text
+  });
 }
 
 module.exports = interviewController;
