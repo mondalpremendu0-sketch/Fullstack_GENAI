c1a11a3c25157203c88587aade10b46be970a6af
Premendu Mondal <mondalpremendu0@gmail.com>
2026-06-03 19:03:51 +0530

    set node env

--- frontend/src/features/Interview/services/interView.api.js
+++ frontend/src/features/Interview/services/interView.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const interviewApi = axios.create({
-  baseURL: "https://premendu.indevs.in" || "https://premendu.indevs.in/",
+  baseURL: "https://premendu.indevs.in",
   withCredentials:true
 })
 
