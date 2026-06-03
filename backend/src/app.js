755ffee3f027270756bf7504cf6443bf85f9ea13
Premendu Mondal <mondalpremendu0@gmail.com>
2026-06-03 21:17:41 +0530

    change g url

--- backend/src/app.js
+++ backend/src/app.js
@@ -32,6 +32,7 @@ app.use(
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
+app.set('trust proxy', 1);
 app.use((req, res, next) => {
     // Manually sanitize body and params, skipping the read-only query object
     if (req.body) {
